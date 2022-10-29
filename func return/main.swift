27c9ec88eafcea56a22a84fb1228dc3a938b5e57
Showaa-06 <115485438+Showaa-06@users.noreply.github.com>
2022-10-29 19:57:39 +0900

    complete

--- func return/main.swift
+++ func return/main.swift
@@ -1,11 +1,16 @@
-//
-//  main.swift
-//  func return
-//
-//  Created by 鈴木彰悟 on 2022/10/29.
-//
+func secretword(word: String) -> Bool {
+    if word == "Open" || word == "Open the Door" {
+        return true
+} else {
+        return false
+    
+}
 
-import Foundation
+}
 
-print("Hello, World!")
+var DoOpen = secretword(word: "Open")
+var DontOpen = secretword(word: "Open~")
+print(DoOpen)
+print(DontOpen)
+
 
